1ce3e02ab77f50f8cefa9dd6820f0094ce717b42
Pierre Neidhardt <mail@ambrevar.xyz>
2020-11-03 16:43:58 +0100

    Version 1.1.0.

--- webfeeder.el
+++ webfeeder.el
@@ -5,7 +5,7 @@
 ;; Author: Pierre Neidhardt <mail@ambrevar.xyz>
 ;; Maintainer: Pierre Neidhardt <mail@ambrevar.xyz>
 ;; URL: https://gitlab.com/Ambrevar/emacs-webfeeder
-;; Version: 1.0.0
+;; Version: 1.1.0
 ;; Package-Requires: ((emacs "25.1"))
 ;; Keywords: news, hypermedia, blog, feed, rss, atom
 
